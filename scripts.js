0c232ff99e762fa51b1d529dcf030f5946001d40
Adrian Romero <radrian031613@gmail.com>
2020-02-09 14:52:10 -0800

    added pseudocode to the scripts.js file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-// 
+//set up function for current day display on jumbotron
+// 1. catch html element (location) and set as variable
+// 2.text() to display changes
+
+
+
+// set up function for current time display on jumbotron
+// 1. catch html element (location) and set as variable
+// 2.text() to display changes
+
+
+
+// set up function to change css depending on time of day
+// 1. for each method to run function through each HTML id 
+// 2. set variable for the css changes (in this case class attributes)
+// 3.attr() changes depending on time (past, current, future)
+
+
+// set up function to save textarea to localstorage
+// 1. .this() on any save button to save textarea
+// 2. set text as variable
+// 3. transfer to local storage
+// 4. make retrievable
