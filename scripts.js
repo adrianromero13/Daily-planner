b01518e3a7d2deb33a8749a0c1d05cfa451ce882
Adrian Romero <radrian031613@gmail.com>
2020-04-11 09:24:32 -0700

    created an on-click  function to save task value to local storage with the id as the key.

--- scripts.js
+++ scripts.js
@@ -1,40 +1,34 @@
-//set up function for current day display on jumbotron
-// 1. catch html element (location) and set as variable
-let currentDate = new Date();
-let dateNow = "Today's Date: " + (currentDate.getMonth()+1) + "/"
-    + currentDate.getDate() + "/"
-    + currentDate.getFullYear();
-// 2.text() to display changes
-$('#currentDay').text(dateNow);
+// make all code wait for html to render first
+$(document).ready(function () {
 
-// same for the current time
-let currentTime = new Date();
-let timeNow = "Time: " + currentTime.getHours() + ":"
-    + currentTime.getMinutes();
-// set text() to #currentTime
-$('#currentTime').text(timeNow);
+    //set up function for current day display on jumbotron
+    let currentDate = new Date();
+    let dateNow = "Today's Date: " + (currentDate.getMonth() + 1) + "/"
+        + currentDate.getDate() + "/"
+        + currentDate.getFullYear();
+    // 2.text() to display changes
+    $('#currentDay').text(dateNow);
+
+    // same for the current time
+    let currentTime = new Date();
+    let timeNow = "Time: " + currentTime.getHours() + ":"
+        + currentTime.getMinutes();
+    // set text() to #currentTime
+    $('#currentTime').text(timeNow);
+
+// saving textarea value into storage at the click of the save button
+    $('.saveBtn').on('click', function () {
+        let $task = $(this).siblings('.description').val();
+        let time = $(this).siblings('.description').attr('id');
+
+        // set the item in localStorage
+        localStorage.setItem(time, $task);
+        // alert the value to check if we got it
+        alert(`${time} task has been saved`);
+
+    });
+
+    // set up function to change css depending on time of day
 
 
-
-// set up function to change css depending on time of day
-// 1. for each method to run function through each HTML id 
-// 2. set variable for the css changes (in this case class attributes)
-// 3.attr() changes depending on time (past, current, future)
-
-
-// set up function to save textarea to localstorage
-// 1. .this() on any save button to save textarea
-// 2. set text as variable
-$('text-center').on('click', function() {
-    let $task = $('textarea').this().text();
-    
-    // set the item in localStorage
-    localStorage.setItem('test', $task);
-    
-    // alert the value to check if we got it
-    console.log(alert(localStorage.getItem('test')));
-    // 3. transfer to local storage
-    // 4. make retrievable
-   
-
-});
+})
